31e865d9908b48674bba4de70528f3601f71d40b
shivrajGunoo <kushalgunoo26081991@gmail.com>
2023-03-27 19:15:40 +0200

    Commiting changes in webhook

--- src/main/java/Application/WebhookApp.java
+++ src/main/java/Application/WebhookApp.java
@@ -1,6 +1,10 @@
 package Application;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
+import java.util.Date;
+import java.util.TimeZone;
 import java.util.logging.Logger;
 
 import org.springframework.boot.SpringApplication;
@@ -14,8 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebhookApp {
 	private static final Logger log = Logger.getLogger(WebhookApp.class.getName());
-
+	private static DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	private static Date date = new Date();
+	
     public static void main(String[] args) {
+    	
+    	df.setTimeZone(TimeZone.getTimeZone("GMT+4"));
+    	
     	String ipAddress = "155.133.23.59"; // Replace with your IP address
     	int port = 80; // Set the port number to 80
     	SpringApplication app = new SpringApplication(WebhookApp.class);
@@ -26,9 +35,10 @@ public class WebhookApp {
 
     @PostMapping("/TradingViewAlerts/alerts")
     public String receiveWebhook(@RequestBody String payload) {
-        // Handle the payload here
-    	System.out.println("Payload recevied");
-    	System.out.println("Received payload: " + payload);
+
+    	log.info("\n");
+    	log.info("Time: " + df.format(date));
+    	log.info("Received payload: " + payload);
         return "Webhook received";
     }
 }
